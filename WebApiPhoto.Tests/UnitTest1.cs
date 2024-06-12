27d827198e532e888868996543ebf8ca4f0b220f
Josorod <romanshemenda@gmail.com>
2024-06-12 21:59:31 +0300

    Added xUnit tests proj

---
+++ WebApiPhoto.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace WebApiPhoto.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            // Initialize objects, set up the test data
+
+            // Act
+            // Call the method to be tested
+
+            // Assert
+            // Verify the result
+
+        }
+    }
+}
